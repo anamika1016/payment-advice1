ad7f529959fd5dda48613cd236bd6f93b55cc356
ANAMIKA VISHWAKARMA <av.anamika1@gmail.com>
2025-03-16 21:41:26 +0530

    send email  changes

--- server/src/controllers/Users.js
+++ server/src/controllers/Users.js
@@ -1,6 +1,11 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import Users from "../models/Users.js";
+import { sendEmail } from "../utils/mailer.js";
+import util from "util";
+import fs from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 
 export const userSignup = async (req, res) => {
   try {
@@ -46,6 +51,23 @@ export const userSignup = async (req, res) => {
     });
 
     await user.save();
+
+    try {
+      const readFile = util.promisify(fs.readFile);
+      const __filename = fileURLToPath(import.meta.url);
+      const __dirname = dirname(__filename);
+      const templatePath = join(__dirname, "../templates/signupTemplate.html");
+      const template = await readFile(templatePath, "utf-8");
+
+      const replcedTemplate = template.replace("${userName}", user.name);
+      -(await sendEmail(
+        process.env.ADMIN_EMAIL,
+        "Welcome to our plateform!",
+        replcedTemplate
+      ));
+    } catch (error) {
+      console.error("Email error:", error);
+    }
 
     const token = jwt.sign(
       { userId: user._id, email: user.email, name: user.name, role: user.role },
@@ -99,6 +121,23 @@ export const userLogin = async (req, res) => {
       return res
         .status(401)
         .send({ success: false, message: "Invalid email or password" });
+    }
+
+    try {
+      const readFile = util.promisify(fs.readFile);
+      const __filename = fileURLToPath(import.meta.url);
+      const __dirname = dirname(__filename);
+      const templatePath = join(__dirname, "../templates/signinTemplate.html");
+      const template = await readFile(templatePath, "utf-8");
+
+      const replcedTemplate = template.replace("${userName}", user.name);
+      -(await sendEmail(
+        process.env.ADMIN_EMAIL,
+        "Login Successful!",
+        replcedTemplate
+      ));
+    } catch (error) {
+      console.error("Email error:", error);
     }
 
     const token = jwt.sign(
---
+++ server/src/utils/mailer.js
@@ -0,0 +1,26 @@
+import dotenv from "dotenv";
+dotenv.config();
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+export const sendEmail = async (to, subject, html) => {
+  try {
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to,
+      subject,
+      html,
+    };
+
+    await transporter.sendMail(mailOptions);
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
+};
